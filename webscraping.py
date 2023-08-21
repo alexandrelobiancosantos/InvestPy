0d5a9142d33d4fd89003508d0f9e5b1f779ded96
alexandrelobiancosantos <brewnext@gmail.com>
2023-08-21 14:46:40 -0300

    webscparoing

--- webscraping.py
+++ webscraping.py
@@ -12,16 +12,46 @@ import time
 
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
 
-driver.get('https://www.fundamentus.com.br/resultado.php')
+
+url = 'https://www.fundamentus.com.br/resultado.php'
+
+driver.get(url)
 
 local_tabela = '/html/body/div[1]/div[2]/table'
-#print(local_tabela)
-tabela = driver.find_element('xpath', local_tabela)
+
+elemento = driver.find_element("xpath", local_tabela)
+
+html_tabela = elemento.get_attribute('outerHTML')
+
+tabela = pd.read_html(str(html_tabela), thousands = '.', decimal = ',')[0]
 #print(tabela)
-html_tabela = tabela.get_attribute('outerHTML')
-#print(html_tabela)
-tabela = pd.read_html(str(html_tabela), thousands='.', decimal=',')
-print(tabela)
 # Adiciona delay para testes do script
 #time.sleep(5)
 
+#set index p nome do ativo
+
+tabela = tabela.set_index('Papel')
+tabela = tabela[['Cotação', 'EV/EBIT', 'ROIC', 'Liq.2meses']]
+#tabela.info()
+tabela['ROIC'] = tabela['ROIC'].str.replace('%','')
+tabela['ROIC'] = tabela['ROIC'].str.replace('.','')
+tabela['ROIC'] = tabela['ROIC'].str.replace(',','.')
+tabela['ROIC'] = tabela['ROIC'].astype(float)
+#print(tabela)
+#tabela.info()
+
+tabela = tabela[tabela['Liq.2meses'] > 1000000]
+#print(tabela)
+
+tabela = tabela[tabela['EV/EBIT'] > 0]
+tabela = tabela[tabela['ROIC'] > 0]
+#print(tabela)
+
+tabela['ranking_ev_ebit'] = tabela['EV/EBIT'].rank(ascending = True)
+tabela['ranking_roic'] = tabela['ROIC'].rank(ascending = False)
+tabela['ranking_final'] = tabela['ranking_ev_ebit'] + tabela['ranking_roic']
+
+print(tabela)
+
+tabela = tabela.sort_values('ranking_final')
+print(tabela.head(10))
